b1ba8a08d0608523a197721153323f7087a577c1
Ralph0210 <ralphchang@utexas.edu>
2023-12-06 01:29:01 -0600

    mroe

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -50,7 +50,7 @@ const Footer = ({ isInView4 }) => {
     // <div >
     <div
       className={styles.footerContainer} ref={footerRef}
-      style={footerStick ? { position: "sticky", bottom: "0", zIndex:"0" } : {}}
+      style={footerStick ? {position:"-webkit-sticky", position: "sticky", bottom: "0", zIndex:"0" } : {}}
     >
       <div
         className={styles.footercta}
