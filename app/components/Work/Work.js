dba92c2577574fa1736eef1476086504d282bf22
Ralph0210 <ralphchang@utexas.edu>
2023-12-06 03:59:41 -0600

    mroe

--- app/components/Work/Work.js
+++ app/components/Work/Work.js
@@ -121,7 +121,7 @@ const Work = ({ changeThemeRef2, footerStick, abilityDeckRef, isInView4 }) => {
 
 
   return (
-    <div ref={changeThemeRef2} className={styles.workContainer} style={{zIndex:"999", position:"relative"}}>
+    <div ref={changeThemeRef2} className={styles.workContainer} style={{zIndex:"2", position:"relative"}}>
       <p ref={footerStick} className={styles.selectedWork}>Selected Work</p>
       <div className={styles.workGallery}>
         <div className={styles.workGalleryTop}>
