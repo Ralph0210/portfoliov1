3f97c69141a6c39238c83fcbce2cb7e0ec0f4b68
Ralph0210 <ralphchang@utexas.edu>
2024-10-28 14:58:29 -0700

    hero.js &apos;

--- app/components/Hero/Hero.js
+++ app/components/Hero/Hero.js
@@ -102,9 +102,9 @@ const Hero = ({ ThemeDark }) => {
           />
         </div> */}
         <div className={styles.heroTextContainer}>
-          <h1 className={` ${lora.className} ${styles.h1}`}>Hi there, I'm Ralph</h1>
+          <h1 className={` ${lora.className} ${styles.h1}`}>Hi there, I&apos;m Ralph</h1>
           {/* <p>I explore the intersection of societal challenges and technology to create purposeful, user-centric experiences that make positive difference.</p> */}
-          <p>I'm a Designer & Developer currently based in Seattle, WA. I believe we deserve innovations that are inclusive, empowering, and rooted in kindness.</p>
+          <p>I&apos;m a Designer & Developer currently based in Seattle, WA. I believe we deserve innovations that are inclusive, empowering, and rooted in kindness.</p>
           <Link href='/contact'>Resume<div className={styles.bounds}></div></Link>
         </div>
       </div>
